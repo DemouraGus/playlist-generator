abc69f12d682b44b50ebb58445dfe14dd4de9658
DemouraGus <demoura.gus@gmail.com>
2025-02-26 13:30:15 -0800

    stablished connections between frontend and backend

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def ping
+    render json: { message: "Hello from Rails!"}
+  end
 end
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("/api/ping")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => console.error("Error:", err));
+  }, []);
 
   return (
     <div className="App">
       <nav>This is the nav bar</nav>
-      
+      <h1>{message || "Loading..."}</h1>
+
     </div>
   )
-}
+};
 
 export default App
